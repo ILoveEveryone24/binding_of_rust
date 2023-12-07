da18bfe692e7f3c7a63b131c79959b0b3eeea132
ILoveEveryone24 <ernestascizas2404@gmail.com>
2023-12-07 22:18:30 +0100

    better movement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate sdl2;
 use sdl2::event::Event;
-use sdl2::keyboard::{Keycode, Scancode};
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
+use std::collections::HashMap;
 use std::time::Duration;
 
 fn main() {
+    let window_width: i32 = 2560;
+    let window_height: i32 = 1600;
     let sdl_context = match sdl2::init() {
         Ok(x) => x,
         Err(error) => panic!("Couldn't initialize sdl context: {:?}", error),
@@ -15,7 +18,7 @@ fn main() {
         Err(error) => panic!("Couldn't initialize video_subsystem: {:?}", error),
     };
     let window = match video_subsystem
-        .window("Binding of rust", 2560, 1600)
+        .window("Binding of rust", window_width as u32, window_height as u32)
         .position_centered()
         .resizable()
         .build()
@@ -36,6 +39,12 @@ fn main() {
 
     let mut player = Rect::new(1280, 800, 100, 100);
     let speed = 15;
+    let mut movement = HashMap::from([
+        ("up", false),
+        ("right", false),
+        ("down", false),
+        ("left", false),
+    ]);
 
     'running: loop {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
@@ -49,26 +58,65 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => *movement.get_mut("up").unwrap() = true,
+                Event::KeyUp {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => *movement.get_mut("up").unwrap() = false,
+                Event::KeyDown {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => *movement.get_mut("right").unwrap() = true,
+                Event::KeyUp {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => *movement.get_mut("right").unwrap() = false,
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => *movement.get_mut("down").unwrap() = true,
+                Event::KeyUp {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => *movement.get_mut("down").unwrap() = false,
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => *movement.get_mut("left").unwrap() = true,
+                Event::KeyUp {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => *movement.get_mut("left").unwrap() = false,
                 _ => {}
             }
         }
-
-        let keyboard_state = event_pump.keyboard_state();
-        match keyboard_state.pressed_scancodes().next() {
-            Some(Scancode::W) => {
-                player.set_y(player.y() - speed);
+        if movement["up"] {
+            player.set_y(player.y() - speed);
+            if player.y() < 0 {
+                player.set_y(0);
             }
-            Some(Scancode::A) => {
-                player.set_x(player.x() - speed);
+        }
+        if movement["right"] {
+            player.set_x(player.x() + speed);
+            if player.x() > window_width {
+                player.set_x(window_width);
             }
-            Some(Scancode::S) => {
-                player.set_y(player.y() + speed);
+        }
+        if movement["down"] {
+            player.set_y(player.y() + speed);
+            if player.y() > window_height {
+                player.set_y(window_height);
             }
-            Some(Scancode::D) => {
-                player.set_x(player.x() + speed);
+        }
+        if movement["left"] {
+            player.set_x(player.x() - speed);
+            if player.x() < 0 {
+                player.set_x(0);
             }
-            _ => {}
-        };
+        }
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
